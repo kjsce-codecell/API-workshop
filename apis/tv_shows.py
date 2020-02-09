02041bd663789093fef309a9060ac79fc00a8125
nachiketbhuta <nachiketbhuta@gmail.com>
2020-02-09 14:08:39 +0530

    #1 bug fixed

--- apis/tv_shows.py
+++ apis/tv_shows.py
@@ -14,10 +14,11 @@ def random_tv_show():
     tv_show = all_tv_shows[r]
     with open('apis/data/tv_shows/' + tv_show, 'r') as f:
         data = json.load(f)
+        genre = ', '.join(data['genres'])
     if is_json is not None:
         return jsonify(data)
     else:
-        return render_template('tv_show.html', tv_shows=data)
+        return render_template('tv_show.html', tv_show=data, genre=genre)
 
 
 @tv_shows.route('/docs')
@@ -27,20 +28,20 @@ def docs():
             'url': '/random',
             'method': 'GET',
             'description': 'Fetch the html page of a random tv shows. ' +
-                'Use /api/tv_shows/random/?json to the data in json format'
+                'Use /api/tv_shows/random?json to the data in json format'
         },
-        # {
-        #     'url': '/add',
-        #     'method': 'POST',
-        #     'description': 'Send a json containing the comic data having '+
-        #         'the following format: {}'.format({
-        #         "name": "Name",
-        #         "title": "Title of the comic",
-        #         "quote_1": "Quote 1",
-        #         "quote_2": "Quote 2",
-        #         "quote_3": "Quote 3"
-        #     })
-        # }
+        {
+            'url': '/add',
+            'method': 'POST',
+            'description': 'Send a json containing the comic data having '+
+                'the following format: {}'.format({
+                "name": "Name",
+                "title": "Title of the comic",
+                "quote_1": "Quote 1",
+                "quote_2": "Quote 2",
+                "quote_3": "Quote 3"
+            })
+        }
     ]
 
     return jsonify({'endpoints': endpoints})
