bffceac0b4fc428d49dd3a91db23d1d4f563664d
nachiketbhuta <nachiketbhuta@gmail.com>
2020-02-09 15:33:22 +0530

    Added new endpoints

--- apis/tv_shows.py
+++ apis/tv_shows.py
@@ -20,6 +20,38 @@ def random_tv_show():
     else:
         return render_template('tv_show.html', tv_show=data, genre=genre)
 
+@tv_shows.route('/add', methods=["POST"])
+def add_tv_show():
+    data = request.get_json()
+    name = data.get('name')
+    try:
+        all_tv_shows = os.listdir('apis/data/tv_shows/')
+        if name + '.json' in all_tv_shows:
+            return jsonify({"message": "Tv show with title:'{}' already exists".format(name)})
+        with open('apis/data/tv_shows/{}.json'.format(name), 'w') as f:
+            json.dump(data, f)
+        return jsonify({"message": "Tv show added successfully"})
+    except Exception as err:
+        return jsonify({"message": "Tv show could not be added", "error": err})
+
+@tv_shows.route('/tv_show', methods=['GET'])
+def get_tv_show():
+    imdb_id = request.args.get('id')
+    print(imdb_id)
+    try:
+        all_tv_shows = os.listdir('apis/data/tv_shows/')
+        data = []
+        for tv_show in all_tv_shows:
+            with open('apis/data/tv_shows/' + tv_show, 'r') as f:
+                json_data = json.load(f)
+                if imdb_id == json_data['imdb_link']:
+                    data = json_data
+                    break
+        return jsonify({ 'data': data })
+    except Exception as err:
+        return jsonify({"message": "Tv show not found", "error": err})
+   
+              
 
 @tv_shows.route('/docs')
 def docs():
@@ -35,12 +67,23 @@ def docs():
             'method': 'POST',
             'description': 'Send a json containing the comic data having '+
                 'the following format: {}'.format({
-                "name": "Name",
-                "title": "Title of the comic",
-                "quote_1": "Quote 1",
-                "quote_2": "Quote 2",
-                "quote_3": "Quote 3"
+                "name": "Name of the TV Show",
+                "premiered": "Date of the premiere in DD-MM-YYYY",
+                "genres": [
+                    "Genre 1",
+                    "Genre 2",
+                ],
+                "runtime": "Average runtime (e.g 57min)",
+                "no_of_seasons": "integer",
+                "imdb_rating": "IMDB rating",
+                "imdb_link": "ID of the IMDB page of the tv show"
             })
+        },
+        {
+            'url': '/tv_show',
+            'method': 'GET',
+            'description': 'Send a json containing the tv show data specified by the id',
+            'query_string': 'id = ' + 'the IMDB id'
         }
     ]
 
