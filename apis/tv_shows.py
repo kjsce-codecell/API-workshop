3012b60c557e09f692e25239944574ac16f5e4af
nachiketbhuta <nachiketbhuta@gmail.com>
2020-02-09 13:27:56 +0530

    Added tv shows api

---
+++ apis/tv_shows.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, request, jsonify, render_template
+import os
+import random
+import json
+import datetime
+
+tv_shows = Blueprint('tv_shows', __name__)
+
+@tv_shows.route('/random')
+def random_tv_show():
+    is_json = request.args.get('json')
+    all_tv_shows = os.listdir('apis/data/tv_shows/')
+    r = random.randint(0, len(all_tv_shows)-1)
+    tv_show = all_tv_shows[r]
+    with open('apis/data/tv_shows/' + tv_show, 'r') as f:
+        data = json.load(f)
+    if is_json is not None:
+        return jsonify(data)
+    else:
+        return render_template('tv_show.html', tv_shows=data)
+
+
+@tv_shows.route('/docs')
+def docs():
+    endpoints = [
+        {
+            'url': '/random',
+            'method': 'GET',
+            'description': 'Fetch the html page of a random tv shows. ' +
+                'Use /api/tv_shows/random/?json to the data in json format'
+        },
+        # {
+        #     'url': '/add',
+        #     'method': 'POST',
+        #     'description': 'Send a json containing the comic data having '+
+        #         'the following format: {}'.format({
+        #         "name": "Name",
+        #         "title": "Title of the comic",
+        #         "quote_1": "Quote 1",
+        #         "quote_2": "Quote 2",
+        #         "quote_3": "Quote 3"
+        #     })
+        # }
+    ]
+
+    return jsonify({'endpoints': endpoints})
